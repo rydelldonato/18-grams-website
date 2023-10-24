e2e97c27e2a2b277a7d256a0c746e20224de5165
Rydell <donatorydell98@gmail.com>
2023-10-23 21:59:58 -0700

    added header address styles

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import "./header.css"
 
 export default function Header() {
   return (
     <>
+    <div className='headerAddress'>
     <p>1600 Ethan Way Ste 30, Sacramento, CA 95825</p>
     <p>9677 E Stockton Blvd, Elk Grove, CA 95624</p>
+    </div>
     </>
   )
 }
