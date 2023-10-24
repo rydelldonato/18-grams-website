ffb6b50e3abec367f4a99c11af525dd2a11520e4
Rydell <donatorydell98@gmail.com>
2023-10-23 21:57:24 -0700

    inital commit

---
+++ src/components/header/header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <>
+    <p>1600 Ethan Way Ste 30, Sacramento, CA 95825</p>
+    <p>9677 E Stockton Blvd, Elk Grove, CA 95624</p>
+    </>
+  )
+}
