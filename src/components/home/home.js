d3a18e016e4425601b1f48f090539d690458b5c7
Rydell <donatorydell98@gmail.com>
2023-10-25 19:26:20 -0700

    home headline added w styling

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,15 +1,24 @@
-import React from 'react'
-import Header from '../header/header'
+import React from "react";
+import Header from "../header/header";
+import "./home.css";
 
 export default function Home() {
   return (
     <>
-    <Header/>
-    <a>
-      <div>
-      <img width={"100%"} src='./o.jpg'/>
-      </div>
+      <Header />
+      <a href="https://www.instagram.com/18grams.916/">
+        <div>
+          <img width={"100%"} src="./o.jpg" />
+        </div>
       </a>
+      <div className="headLineContainer">
+        <p>Midtown-Inspired</p>
+        <img src="./icons-coffee.png" />
+        <p>Crafted In-House</p>
+        <img src="./icons-coffee.png" />
+        <p>Sip-Worthy</p>
+        <img src="./icons-coffee.png" />
+      </div>
     </>
-  )
+  );
 }
