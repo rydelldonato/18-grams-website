cfaaebe85f75f8a51b7fdf7963bbfd9ce3166267
Rydell <donatorydell98@gmail.com>
2023-11-02 19:34:21 -0700

    slideshow test

--- src/components/slideshows/hoursAndLocation/slideshow.js
+++ src/components/slideshows/hoursAndLocation/slideshow.js
@@ -1,21 +1,35 @@
 import React from "react";
-import { Slide } from 'react-slideshow-image';
-import './slideshow.css'
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+import "./slideshow.css";
 
-const images = [
-  '/frontCounter.jpg',
-//   'image2.jpg',
-//   'image3.jpg',
-  // Add more image paths here
+const slideImages = [
+  {
+    url: "https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+    caption: "Slide 1",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto.format&fit=crop&w=1536&q=80",
+    caption: "Slide 2",
+  },
+  {
+    url: "https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80",
+    caption: "Slide 3",
+  },
 ];
 
 const Slideshow = () => {
   return (
-    <div className="slideshow-container">
-      <Slide images={images}>
-        {images.map((each, index) => (
+    <div className="slide-container">
+      <Slide>
+        {slideImages.map((slideImage, index) => (
           <div key={index}>
-            <img src={each} alt={`Slide ${index}`} />
+            <div
+              className="slide-div"
+              style={{ backgroundImage: `url(${slideImage.url})` }}
+            >
+              <span className="slide-span">{slideImage.caption}</span>
+            </div>
           </div>
         ))}
       </Slide>
