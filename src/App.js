7df2a98c32204748228746dec25ca3e7045d6d5b
Rydell <donatorydell98@gmail.com>
2023-11-05 21:37:54 -0800

    added scroll to top

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./components/home/home";
 import HoursAndLocation from "./components/hoursAndLocation/hoursAndLocation";
+import ScrollToTop from "./components/scrollToTop/scrollToTop";
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <ScrollToTop/>
         <Routes>
           <Route exact path="/" element={<Home/>} />
           <Route exact path="/hoursandlocation" element={<HoursAndLocation/>} />
