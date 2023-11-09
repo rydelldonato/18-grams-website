72455513094da18160b95793ab62f1a3691f511c
Rydell <donatorydell98@gmail.com>
2023-11-08 21:10:34 -0800

    logo change

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -11,7 +11,7 @@ export default function Home() {
         <div className="topHomeSection">
           <a href="https://www.instagram.com/18grams.916/">
             <div className="storeLogo">
-              <img alt="store logo" width="90%" src="./o.jpg" />
+              <img alt="store logo" width="90%" src="./crop.png" />
             </div>
           </a>
           <div className="headLineContainer">
