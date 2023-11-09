194968e4e7c0e9c6f46ac4112dee9a14f50077c0
Rydell <donatorydell98@gmail.com>
2023-11-08 22:30:31 -0800

    added background to button

--- src/components/hoursAndLocation/hoursAndLocation.js
+++ src/components/hoursAndLocation/hoursAndLocation.js
@@ -5,6 +5,23 @@ import Slideshow from "../slideshows/hoursAndLocation/slideshow";
 export default function HoursAndLocation() {
   const isMobileDevice = /Mobi|Android/i.test(navigator.userAgent);
 
+  function handleDirectionsButton() {
+    // Define the Google Maps URL
+    const googleMapsURL =
+      "https://www.google.com/maps/search/?api=1&query=1600%20Ethan%20Way%20Ste%2030%2C%20Sacramento%2C%20CA%2095825";
+
+    if (isMobileDevice) {
+      // If it's a mobile device, open a mobile-friendly link
+      window.open(
+        "maps:?q=1600+Ethan+Way+Ste+30,+Sacramento,+CA+95825",
+        "_blank"
+      );
+    } else {
+      // If it's not a mobile device, open the Google Maps URL
+      window.open(googleMapsURL, "_blank");
+    }
+  }
+
   return (
     <>
       <Header />
@@ -20,34 +37,32 @@ export default function HoursAndLocation() {
           <p>Open every day of the week!</p>
           <p>Check us out on Yelp!</p>
           <div style={{ height: 20 }}></div>
-          <p>
-            {isMobileDevice ? (
-              <a
-                href="maps:?q=1600+Ethan+Way+Ste+30,+Sacramento,+CA+95825"
-              >
-                1600 Ethan Way Ste 30, Sacramento, CA 95825
-              </a>
-            ) : (
-              <a
-                href="https://www.google.com/maps/search/?api=1&query=1600%20Ethan%20Way%20Ste%2030%2C%20Sacramento%2C%20CA%2095825"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                1600 Ethan Way Ste 30, Sacramento, CA 95825
-              </a>
-            )}
-          </p>
+          <p>1600 Ethan Way Ste 30, Sacramento, CA 95825</p>
           <p>M-W 8AM-2PM, TH-Sun 8AM-8PM</p>
           {/* Yelp Review Embed Code */}
-          <span
+          {/* <span
             className="yelp-review"
             data-review-id="zaOSDECRx_6YO30jJf0fRw"
             data-hostname="www.yelp.com"
             style={{ height: 50 }}
           >
             Read <a href="https://www.yelp.com/user_details?userid=CNl758-IFadffzzaKH9eUw" rel="nofollow noopener">Athena M.</a>'s <a href="https://www.yelp.com/biz/18-grams-coffee-roasters-sacramento?hrid=zaOSDECRx_6YO30jJf0fRw" rel="nofollow noopener">review</a> of <a href="https://www.yelp.com/biz/rtF0Mvs6iScYR9oBueQrtA" rel="nofollow noopener">18 Grams Coffee Roasters</a> on <a href="https://www.yelp.com" rel="nofollow noopener">Yelp</a>
-          </span>
+          </span> */}
           <div style={{ height: 15 }}></div>
+        </div>
+        <div
+          style={{
+            backgroundImage: 'url("/maps.png")',
+            height: "215px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          className="getDirectionsButton"
+        >
+          <button onClick={() => handleDirectionsButton()}>
+            Get Directions
+          </button>
         </div>
       </div>
     </>
