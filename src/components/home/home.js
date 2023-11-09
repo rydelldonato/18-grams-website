bb85a05a33d7fb12ac45d0a3747d6d56e508bc20
Rydell <donatorydell98@gmail.com>
2023-11-08 20:58:27 -0800

    font change

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -6,30 +6,32 @@ import DownArrow from "../downArrow/downArrow";
 export default function Home() {
   return (
     <>
-    <div style={{height:'100vh'}}>
-      <Header />
-      <a href="https://www.instagram.com/18grams.916/">
-        <div>
-          <img alt="store logo" width="90%" src="./o.jpg" />
+      <div>
+        <Header />
+        <div className="topHomeSection">
+          <a href="https://www.instagram.com/18grams.916/">
+            <div className="storeLogo">
+              <img alt="store logo" width="90%" src="./o.jpg" />
+            </div>
+          </a>
+          <div className="headLineContainer">
+            <p>Midtown-Inspired</p>
+            <img alt="coffee icon" src="./icons-coffee.png" />
+            <p>Crafted In-House</p>
+            <img alt="coffee icon" src="./icons-coffee.png" />
+            <p>Sip-Worthy</p>
+            <img alt="coffee icon" src="./icons-coffee.png" />
+          </div>
+          <DownArrow targetDivId="target-div" />
+          <div id="target-div">
+            <img className="canva" alt="food and drinks" src="./page1.gif" />
+            <img className="canva" alt="food and drinks" src="./page2.gif" />
+            <img className="canva" alt="food and drinks" src="./page3.gif" />
+            <img className="canva" alt="food and drinks" src="./page4.gif" />
+            <img className="canva" alt="food and drinks" src="./page5.gif" />
+          </div>
         </div>
-      </a>
-      <div className="headLineContainer">
-        <p>Midtown-Inspired</p>
-        <img alt="coffee icon" src="./icons-coffee.png" />
-        <p>Crafted In-House</p>
-        <img alt="coffee icon" src="./icons-coffee.png" />
-        <p>Sip-Worthy</p>
-        <img alt="coffee icon" src="./icons-coffee.png" />
       </div>
-      <DownArrow targetDivId="target-div" />
-      <div id="target-div">
-        <img className="canva" alt="food and drinks" src="./page1.gif"/>
-        <img className="canva" alt="food and drinks" src="./page2.gif"/>
-        <img className="canva" alt="food and drinks" src="./page3.gif"/>
-        <img className="canva" alt="food and drinks" src="./page4.gif"/>
-        <img className="canva" alt="food and drinks" src="./page5.gif"/>
-      </div>
-    </div>
     </>
   );
 }
