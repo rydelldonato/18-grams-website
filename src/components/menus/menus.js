51ff3a09c986741484b9b549a0e1c434d8472a2f
Rydell <donatorydell98@gmail.com>
2023-11-10 09:32:05 -0800

    added menus text to slideshow

--- src/components/menus/menus.js
+++ src/components/menus/menus.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from "react";
+import './menus.css'
+import Header from "../header/header";
+import Slideshow from "../slideshows/hoursAndLocation/slideshow";
 
 export default function Menus() {
+  const slideImages = [
+    {
+      url: "./frontCounter.jpg",
+      caption: "Slide 1",
+    },
+    {
+      url: "./lineView.jpg",
+      caption: "Slide 2",
+    },
+    {
+      url: "./murals.jpg",
+      caption: "Slide 3",
+    },
+    {
+      url: "./outsideView.jpg",
+      caption: "Slide 4",
+    },
+  ];
   return (
-    <div>menu</div>
-  )
+    <>
+      <Header />
+      <div>
+        <div className="menusSpanDiv">
+        <span className="menusSpan"><h1>Menus</h1></span>
+        </div>
+        <Slideshow slideImages={slideImages} />
+      </div>
+    </>
+  );
 }
