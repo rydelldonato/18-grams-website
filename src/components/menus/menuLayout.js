71031a2614a5db9895647037a45cdd3e27756839
Rydell <donatorydell98@gmail.com>
2023-11-13 13:59:41 -0800

    added categories, coffee beans, spacing

--- src/components/menus/menuLayout.js
+++ src/components/menus/menuLayout.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React from "react";
+import "./menuLayout.css";
 
 export default function MenuLayout(props) {
-const {menu} = props
+  const { menu } = props;
 
-    return (
-      <>
+  return (
+    <>
       {menu.map((drink, index) => (
-        <div key={index}>
+        <div className="menuItemDiv" key={index}>
           {index === 0 || drink.category !== menu[index - 1].category ? (
-            <p>{drink.category}</p>
+            <div className="categoryDiv">
+              <div>
+                <img alt="coffee-beans" src="./icons-coffee-beans.png" />
+              </div>
+              <h3 className="category">{drink.category}</h3>
+            </div>
           ) : null}
           <p>Name: {drink.name}</p>
           <p>Info: {drink.info}</p>
@@ -16,5 +22,5 @@ const {menu} = props
         </div>
       ))}
     </>
-    );
-  }
+  );
+}
