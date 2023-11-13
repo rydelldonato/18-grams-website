bc0251910e5bc889e55d49c3bc52b7c0a20904e5
Rydell <donatorydell98@gmail.com>
2023-11-13 13:40:35 -0800

    added categories and functionality to js

--- src/components/menus/drinks/drinks.js
+++ src/components/menus/drinks/drinks.js
@@ -1,10 +1,54 @@
 const drinks = [
     {
+        category:'Traditional',
         name: 'coffee',
         info: 'coffee',
         price: 2
     },
     {
+        category:'House Specials',
+        name: 'espresso',
+        info: 'coffee',
+        price: 2
+    },
+    {
+        category:'Mocha',
+        name: 'tea',
+        info: 'tea',
+        price: 2
+    },
+    {
+        category:'Special Lattes',
+        name: 'tea',
+        info: 'tea',
+        price: 2
+    },
+    {
+        category:'Tea Lattes',
+        name: 'tea',
+        info: 'tea',
+        price: 2
+    },
+    {
+        category:'Matcha',
+        name: 'tea',
+        info: 'tea',
+        price: 2
+    },
+    {
+        category:'House Iced Tea',
+        name: 'tea',
+        info: 'tea',
+        price: 2
+    },
+    {
+        category:'Loose Leaf Tea',
+        name: 'tea',
+        info: 'tea',
+        price: 2
+    },
+    {
+        category:'Lemonade',
         name: 'tea',
         info: 'tea',
         price: 2
--- src/components/menus/menuLayout.js
+++ src/components/menus/menuLayout.js
@@ -5,13 +5,16 @@ const {menu} = props
 
     return (
       <>
-        {menu.map((drink, index) => (
-          <div key={index}>
-            <p>Name: {drink.name}</p>
-            <p>Info: {drink.info}</p>
-            <p>Price: ${drink.price}</p>
-          </div>
-        ))}
-      </>
+      {menu.map((drink, index) => (
+        <div key={index}>
+          {index === 0 || drink.category !== menu[index - 1].category ? (
+            <p>{drink.category}</p>
+          ) : null}
+          <p>Name: {drink.name}</p>
+          <p>Info: {drink.info}</p>
+          <p>Price: ${drink.price}</p>
+        </div>
+      ))}
+    </>
     );
   }
