2d9f35b2cd8745eab4ebea8df0d5cfed9b5239d5
Rydell <donatorydell98@gmail.com>
2023-11-17 01:15:48 -0800

    added brunch items to skillet

--- src/components/menus/brunch/brunch.js
+++ src/components/menus/brunch/brunch.js
@@ -1,14 +1,95 @@
 const brunch = [
-    {
-        name: 'eggs',
-        info: 'eggs',
-        price: 2
-    },
-    {
-        name: 'tea',
-        info: 'tea',
-        price: 2
-    },
-]
+  {
+    category: "Breakfast",
+    name: "LOX BAGEL",
+    info: "Smoked salmon lox, capers, chive cream cheese, and red onions",
+    price: 10.5,
+  },
+  {
+    category: "Breakfast",
+    name: "EGG BREAKFAST SANDO",
+    info: "Folded eggs with green onions, melted cheese, garlic aioll, on a brioche bun",
+    price: 9,
+  },
+  {
+    category: "Breakfast",
+    name: "CHILAQUILES",
+    info: `Choice of: Salsa Roja | Salsa Verde.
+    Fried tortilla chip simmered in salsa of
+    choice, Topped with two over easy eggs,
+    cheese, onions, and sour cream`,
+    price: 15.5,
+  },
+  {
+    category: "Breakfast",
+    name: "HONG KONG FRENCH TOAST",
+    info: `Choice of: Ube Cream Cheese | Nutella.
+    Panko battered Pullman bread stuffed with
+    flavor of choice, drizzled with condensed
+    milk, and topped with seasonal fruit and
+    house made whipped cream`,
+    price: 13,
+  },
+  {
+    category: "Sandwiches",
+    name: "KFC",
+    info: "Korean fried chicken inspired sando with a sweet gochujang pepper sauce, spicy mayo, and slaw on a toasted broche bun. Served with potato wedges",
+    price: 15.5,
+  },
+  {
+    category: "Sandwiches",
+    name: "CHIPOTLE CHICKEN CLUB",
+    info: "Chicken, bacon, lettuce, and tomnato with chipotle mayo on French broad Served with potato wedges.",
+    price: 14,
+  },
+  {
+    category: "Sandwiches",
+    name: "LONGANISA",
+    info: "Longanisa sausage patty, sunny side up egg, jack cheese, banana ketchup and jalepeno on a brioche bun. Served with potato wedges.",
+    price: 15.5,
+  },
+  {
+    category: "Sandwiches",
+    name: "HOT KARAAGE",
+    info: "Spicy Japanese style fried chickon scrambled egg, swiss cheese, and garlic aioli on buttered toast. Served with potato wedges",
+    price: 16.5,
+  },
+  {
+    category: "Sandwiches",
+    name: "SAIGON CHICKEN",
+    info: "Fried chicken thigh coated in a sweet garlic fish sauce. Topped with pickied dailkon and carrots and fresh cilantro on buttered toast. Served with a side salad",
+    price: 15.5,
+  },
+  {
+    category: "Plates",
+    name: "LOCOMOCO",
+    info: "Smashed beef patty, onions, sweet gravy, sunny side up egg, micro cilantro ard truffle salt served over rice",
+    price: 15.5,
+  },
+  {
+    category: "Plates",
+    name: "RICE BOWL",
+    info: "Saigon Chicken | Katsu Chicken | Longanisa",
+    price: 10.8,
+  },
+  {
+    category: "Skillets",
+    name: "SISIG SKILLET",
+    info: "Crispy pork belly and green onion topped with pickled red onion, aioli, egg yok. Served with a side of garic rice.",
+    price: 18,
+  },
+  {
+    category: "Skillets",
+    name: "STEAK AND EGGS",
+    info: "8 oz. New York trio steak topped witn fried garlic and shallots. Served with a sunny side up egg, steamod rice, and house sauce.",
+    price: 20,
+  },
+  {
+    category: "Skillets",
+    name: "BREAKFAST SKILLET",
+    info: "Scrambled egg, potato, sausage, bacon, and longanisa in chili oil topped with cheese, jalapenos and green onions. Served with toasted bread",
+    price: 16,
+  },
+];
 
-export default brunch
+export default brunch;
