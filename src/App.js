7ff43c6601788defef8bbd1019eb72e4f79f74bf
Rydell <donatorydell98@gmail.com>
2023-11-17 10:24:16 -0800

    created contact page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/home/home";
 import HoursAndLocation from "./components/hoursAndLocation/hoursAndLocation";
 import ScrollToTop from "./components/scrollToTop/scrollToTop";
 import Menus from "./components/menus/menus";
+import Contact from "./components/contact/contact";
 function App() {
   return (
     <BrowserRouter>
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/hoursandlocation" element={<HoursAndLocation />} />
           <Route path="/menus" element={<Menus />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
     </BrowserRouter>
