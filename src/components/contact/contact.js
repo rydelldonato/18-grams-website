5732be3712790dba698890deaaabe4c52d2267da
Rydell <donatorydell98@gmail.com>
2023-11-20 13:22:01 -0800

    contact form initial commit

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import Header from '../header/header'
+import React from 'react';
+import Header from '../header/header';
 
 export default function Contact() {
+
   return (
     <>
-    <Header/>
+      <Header />
+      <iframe
+        id="JotFormIFrame-233218218954156"
+        title="18 grams contact form"
+        onLoad={() => window.parent.scrollTo(0, 0)}
+        allowtransparency="true"
+        allowFullScreen="true"
+        allow="geolocation; microphone; camera"
+        src="https://form.jotform.com/233218218954156"
+        style={{ minWidth: '100%', maxWidth: '100%', height: '539px', border: 'none' }}
+      >
+      </iframe>
     </>
-  )
+  );
 }
