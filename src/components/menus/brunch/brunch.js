7acdf35e8ceb237570d9c547cd21d307779c2c9e
rydelldonato <rydell.donato@gmail.com>
2024-01-17 23:18:13 -0800

    corrected spelling

--- src/components/menus/brunch/brunch.js
+++ src/components/menus/brunch/brunch.js
@@ -8,7 +8,7 @@ const brunch = [
   {
     category: "Breakfast",
     name: "EGG BREAKFAST SANDO",
-    info: "Folded eggs with green onions, melted cheese, garlic aioll, on a brioche bun",
+    info: "Folded eggs with green onions, melted cheese, garlic aioli, on a brioche bun",
     price: 9,
   },
   {
@@ -27,43 +27,43 @@ const brunch = [
     Panko battered Pullman bread stuffed with
     flavor of choice, drizzled with condensed
     milk, and topped with seasonal fruit and
-    house made whipped cream`,
+    house-made whipped cream`,
     price: 13,
   },
   {
     category: "Sandwiches",
     name: "KFC",
-    info: "Korean fried chicken inspired sando with a sweet gochujang pepper sauce, spicy mayo, and slaw on a toasted broche bun. Served with potato wedges",
+    info: "Korean fried chicken-inspired sando with a sweet gochujang pepper sauce, spicy mayo, and slaw on a toasted brioche bun. Served with potato wedges",
     price: 15.5,
   },
   {
     category: "Sandwiches",
     name: "CHIPOTLE CHICKEN CLUB",
-    info: "Chicken, bacon, lettuce, and tomnato with chipotle mayo on French broad Served with potato wedges.",
+    info: "Chicken, bacon, lettuce, and tomato with chipotle mayo on French bread. Served with potato wedges.",
     price: 14,
   },
   {
     category: "Sandwiches",
     name: "LONGANISA",
-    info: "Longanisa sausage patty, sunny side up egg, jack cheese, banana ketchup and jalepeno on a brioche bun. Served with potato wedges.",
+    info: "Longanisa sausage patty, sunny-side-up egg, jack cheese, banana ketchup, and jalapeno on a brioche bun. Served with potato wedges.",
     price: 15.5,
   },
   {
     category: "Sandwiches",
     name: "HOT KARAAGE",
-    info: "Spicy Japanese style fried chickon scrambled egg, swiss cheese, and garlic aioli on buttered toast. Served with potato wedges",
+    info: "Spicy Japanese-style fried chicken, scrambled egg, Swiss cheese, and garlic aioli on buttered toast. Served with potato wedges",
     price: 16.5,
   },
   {
     category: "Sandwiches",
     name: "SAIGON CHICKEN",
-    info: "Fried chicken thigh coated in a sweet garlic fish sauce. Topped with pickied dailkon and carrots and fresh cilantro on buttered toast. Served with a side salad",
+    info: "Fried chicken thigh coated in a sweet garlic fish sauce. Topped with pickled daikon and carrots and fresh cilantro on buttered toast. Served with a side salad",
     price: 15.5,
   },
   {
     category: "Plates",
     name: "LOCOMOCO",
-    info: "Smashed beef patty, onions, sweet gravy, sunny side up egg, micro cilantro ard truffle salt served over rice",
+    info: "Smashed beef patty, onions, sweet gravy, sunny-side-up egg, micro cilantro, and truffle salt served over rice",
     price: 15.5,
   },
   {
@@ -75,19 +75,19 @@ const brunch = [
   {
     category: "Skillets",
     name: "SISIG SKILLET",
-    info: "Crispy pork belly and green onion topped with pickled red onion, aioli, egg yok. Served with a side of garic rice.",
+    info: "Crispy pork belly and green onion topped with pickled red onion, aioli, egg yolk. Served with a side of garlic rice.",
     price: 18,
   },
   {
     category: "Skillets",
     name: "STEAK AND EGGS",
-    info: "8 oz. New York trio steak topped witn fried garlic and shallots. Served with a sunny side up egg, steamod rice, and house sauce.",
+    info: "8 oz. New York strip steak topped with fried garlic and shallots. Served with a sunny-side-up egg, steamed rice, and house sauce.",
     price: 20,
   },
   {
     category: "Skillets",
     name: "BREAKFAST SKILLET",
-    info: "Scrambled egg, potato, sausage, bacon, and longanisa in chili oil topped with cheese, jalapenos and green onions. Served with toasted bread",
+    info: "Scrambled egg, potato, sausage, bacon, and longanisa in chili oil topped with cheese, jalapenos, and green onions. Served with toasted bread",
     price: 16,
   },
   {
