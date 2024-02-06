86c5abde829ab9c9cc33c967ddee594f89911aaf
rydelldonato <rydell.donato@gmail.com>
2024-02-05 20:46:03 -0800

    added keywords to header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -12,6 +12,7 @@ export default function Header() {
   return (
     <>
       <div className="topHeader">
+        <p>18 Grams Coffee Roasters / 18 Grams Coffee + Tea</p>
         <p>1600 Ethan Way Ste 30, Sacramento, CA 95825</p>
         <div className="menuBarsContainer">
           <div
