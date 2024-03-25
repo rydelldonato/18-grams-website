e692f746c69f074790ee37861e70df48c27f65c4
rydelldonato <rydell.donato@gmail.com>
2024-03-25 14:34:42 -0700

    updated brunch menu & added filter button components

--- src/components/menus/brunch/brunch.js
+++ src/components/menus/brunch/brunch.js
@@ -1,9 +1,15 @@
 const brunch = [
   {
     category: "Breakfast",
+    name: "Avocado Toast",
+    info: "Sliced avocado and cherry tomatoes on toast, topped with a poached egg",
+    price: 8,
+  },
+  {
+    category: "Breakfast",
     name: "LOX BAGEL",
     info: "Smoked salmon lox, capers, chive cream cheese, and red onions",
-    price: 10.5,
+    price: 11,
   },
   {
     category: "Breakfast",
@@ -32,45 +38,57 @@ const brunch = [
   },
   {
     category: "Sandwiches",
+    name: "Avocado BLT",
+    info: "Avocado, bacon, lettuce, tomato, and aioli on sourdough bread",
+    price: 12,
+  },
+  {
+    category: "Sandwiches",
+    name: "Brisket Sando",
+    info: "Brisket, swiss cheese, roasted pepper aioli and cucumber on french bread. Served with waffle fries",
+    price: 17,
+  },
+  {
+    category: "Sandwiches",
     name: "KFC",
     info: "Korean fried chicken-inspired sando with a sweet gochujang pepper sauce, spicy mayo, and slaw on a toasted brioche bun. Served with potato wedges",
-    price: 15.5,
+    price: 16.2,
   },
   {
     category: "Sandwiches",
     name: "CHIPOTLE CHICKEN CLUB",
     info: "Chicken, bacon, lettuce, and tomato with chipotle mayo on French bread. Served with potato wedges.",
-    price: 14,
+    price: 14.7,
   },
   {
     category: "Sandwiches",
     name: "LONGANISA",
     info: "Longanisa sausage patty, sunny-side-up egg, jack cheese, banana ketchup, and jalapeno on a brioche bun. Served with potato wedges.",
-    price: 15.5,
+    price: 16.2,
   },
   {
     category: "Sandwiches",
     name: "HOT KARAAGE",
     info: "Spicy Japanese-style fried chicken, scrambled egg, Swiss cheese, and garlic aioli on buttered toast. Served with potato wedges",
-    price: 16.5,
+    price: 17.3,
   },
   {
     category: "Sandwiches",
     name: "SAIGON CHICKEN",
     info: "Fried chicken thigh coated in a sweet garlic fish sauce. Topped with pickled daikon and carrots and fresh cilantro on buttered toast. Served with a side salad",
-    price: 15.5,
+    price: 16.2,
   },
   {
     category: "Plates",
     name: "LOCOMOCO",
     info: "Smashed beef patty, onions, sweet gravy, sunny-side-up egg, micro cilantro, and truffle salt served over rice",
-    price: 15.5,
+    price: 16.3,
   },
   {
     category: "Plates",
     name: "RICE BOWL",
     info: "Saigon Chicken | Katsu Chicken | Longanisa",
-    price: 10.8,
+    price: 11.5,
   },
   {
     category: "Skillets",
@@ -88,25 +106,19 @@ const brunch = [
     category: "Skillets",
     name: "BREAKFAST SKILLET",
     info: "Scrambled egg, potato, sausage, bacon, and longanisa in chili oil topped with cheese, jalapenos, and green onions. Served with toasted bread",
-    price: 16,
+    price: 16.8,
   },
   {
     category: "Udon",
     name: "Miso Carbonara",
     info: "Bacon with gochujang and mayo sauce, green onion, topped with cheese and egg yolk",
-    price: 16,
+    price: 16.8,
   },
   {
     category: "Udon",
     name: "Kimchi Udon",
     info: "Pork Belly, garlic, green onions, topped with fried egg, seaweed, and sesame seeds",
-    price: 15,
-  },
-  {
-    category: "Soup",
-    name: "Leek Potato Soup",
-    info: "Choice of: Side | Bowl. Leeks and potatoes with creamy garlic sauce and our house signature chorizo. Served with toasted bread.",
-    price: '5.99 | 6.99',
+    price: 16,
   },
 ];
 
---
+++ src/components/menus/menuFilterButtons/brunchFilter/brunchButtons.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function brunchButtons() {
+  return (
+    <div>
+      <button>Breakfast</button>
+      <button>Sandwiches</button>
+      <button>Plates</button>
+      <button>Skillets</button>
+      <button>Udon</button>
+    </div>
+  )
+}
